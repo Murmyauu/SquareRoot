112d430460d1087d1d3d12f7c45f9115a0fc4ba5
Munira <munira.ablyazova@gmail.com>
2022-12-17 18:11:20 +0500

    the values of variables in the method and input parameters have been changed

--- src/main/java/ru/netology/squareroot/services/SQRService.java
+++ src/main/java/ru/netology/squareroot/services/SQRService.java
@@ -3,11 +3,13 @@ package ru.netology.squareroot.services;
 public class SQRService {
     public int calcSqrtRoot(int lowerLimit, int upperLimit) {
         int counter = 0;
-        for (int i = 1; i <= upperLimit; i++) {
-            if (i * i >= lowerLimit)
-                if (i * i <= upperLimit) {
-                    counter = counter + 1;
-                }
+        int max = 99;
+        int i = 10;
+        for (i = 10; i <= max; i++) {
+            int root = i * i;
+            if (root >= lowerLimit && root <= upperLimit) {
+                counter = counter + 1;
+            }
         }
         return counter;
     }
